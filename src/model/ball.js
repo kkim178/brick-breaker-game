ed23ce4eba8d1ad5a091e83eacd17b313e18e446
Kyle K <kkim178@jh.edu>
2026-02-18 21:58:52 -0500

    brick-08: Refine ball bounce logic and fix edge collision

---
+++ src/model/ball.js
@@ -0,0 +1,23 @@
+import Sprite from "./sprite.js";
+
+class Ball extends Sprite {
+    constructor(x, y, width, height, color, dx, dy) {
+        super(x, y, width, height, color, dx, dy);
+    }
+
+    bounce(canvasWidth, canvasHeight) {
+        if (this.y < 0 || this.y + this.height > canvasHeight) {
+            // bounce off the left/right edges
+            this.dx *= -1; // switch direction
+        }
+
+        if (this.x < 0 || this.x + this.width > canvasWidth) {
+            // bounce off the top/bottom edge
+            this.dy *= -1; // switch direction
+        }
+    }
+
+
+}
+
+export default Ball;
