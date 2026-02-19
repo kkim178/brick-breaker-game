3b4b713fc67618d5c2cca7487485d8cba934a097
Kyle K <kkim178@jh.edu>
2026-02-18 20:04:55 -0500

    brick-05: Add Sprite class extending Block with movement

--- src/main.js
+++ src/main.js
@@ -1,1 +1,30 @@
 import "./style.css";
+import Block from "./model/block.js"
+
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+
+
+const redBlock = new Block(20, 40, 50, 50, "#FF0000");
+redBlock.draw(ctx);
+
+
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+const dx = 2;
+const dy = -2;
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.beginPath();
+  ctx.rect(x, y, 10, 10);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+  x += dx;
+  y += dy;
+
+  window.requestAnimationFrame(draw);
+}
+
+//draw();
